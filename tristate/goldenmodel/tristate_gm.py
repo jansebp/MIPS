bf1f88eed802acf7f13d84993ad106ce90e36c4a
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-23 03:43:54 -0300

    Refatoração do Código do TRISTATE

--- tristate/goldenmodel/tristate_gm.py
+++ tristate/goldenmodel/tristate_gm.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import os
 import pathlib
 
 from utils import constants, modules
 
 pathlib.Path(constants.TV_PATH).mkdir(parents=True, exist_ok=True)
 pathlib.Path(constants.TB_PATH).mkdir(parents=True, exist_ok=True)
+FILENAME = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0].split('_')[0]
 
-MAX_BIT_VALUE = constants.MAX_VALUE_BITS_1
+N_BITS = 1
+MAX_BIT_VALUE = constants.MAX_VALUE_BITS.get(N_BITS)
 formatter = '0' + str(MAX_BIT_VALUE.bit_length()) + 'b'
 
-with open(constants.TV_PATH + constants.TRISTATE_TV_NAME, 'w') as f:
+with open(constants.TV_PATH + constants.TV_NAME.get(FILENAME), 'w') as f:
     for a in range(0, MAX_BIT_VALUE + 1):
-        for en in range(0, constants.ENABLE_RANGE):
+        for en in range(0, constants.RANGES.get('enable')):
             out = modules.Tristate.tristate(a, en)
             if type(out) == str:
                 f.write(format(a, formatter) + constants.DELIMITER + str(en) + constants.DELIMITER + (
-                            out * MAX_BIT_VALUE.bit_length()) + '\n')
+                        out * MAX_BIT_VALUE.bit_length()) + '\n')
             else:
                 f.write(format(a, formatter) + constants.DELIMITER + str(en) + constants.DELIMITER +
                         format(out, formatter) + '\n')
