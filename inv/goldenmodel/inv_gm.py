b678c46f43258db1ac14b2c384829dc18cb4536c
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-23 03:40:31 -0300

    Refatoração do Código do INV

--- inv/goldenmodel/inv_gm.py
+++ inv/goldenmodel/inv_gm.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import pathlib
+import os
 
 from utils import constants, modules
 
 pathlib.Path(constants.TV_PATH).mkdir(parents=True, exist_ok=True)
 pathlib.Path(constants.TB_PATH).mkdir(parents=True, exist_ok=True)
+FILENAME = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0].split('_')[0]
 
-MAX_BIT_VALUE = constants.MAX_VALUE_BITS_1
+N_BITS = 1
+MAX_BIT_VALUE = constants.MAX_VALUE_BITS.get(N_BITS)
 formatter = '0' + str(MAX_BIT_VALUE.bit_length()) + 'b'
 
-with open(constants.TV_PATH + constants.INV_TV_NAME, 'w') as f:
+with open(constants.TV_PATH + constants.TV_NAME.get(FILENAME), 'w') as f:
     for i in range(0, MAX_BIT_VALUE + 1):
         f.write(format(i, formatter) + constants.DELIMITER + format(modules.Inv.inverter(i, MAX_BIT_VALUE),
                                                                     formatter) + '\n')
