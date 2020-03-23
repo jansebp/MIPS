eaade9f2c6e8c48a687cd842faa5f63e3f7de183
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-23 03:36:39 -0300

    Refatoração do Código

--- decoder/goldenmodel/decoder_gm.py
+++ decoder/goldenmodel/decoder_gm.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
 import pathlib
 
 from utils import constants, modules
 
 pathlib.Path(constants.TV_PATH).mkdir(parents=True, exist_ok=True)
 pathlib.Path(constants.TB_PATH).mkdir(parents=True, exist_ok=True)
+FILENAME = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0].split('_')[0]
 
-MAX_BIT_VALUE = constants.MAX_VALUE_BITS_5
-MAX_BIT_VALUE_OUTPUT = constants.MAX_VALUE_BITS_32
+N_BITS = 5
+N_BITS_OUT = 32
+MAX_BIT_VALUE = constants.MAX_VALUE_BITS.get(N_BITS)
+MAX_BIT_VALUE_OUTPUT = constants.MAX_VALUE_BITS.get(N_BITS_OUT)
 formatter = '0' + str(MAX_BIT_VALUE.bit_length()) + 'b'
 
-with open(constants.TV_PATH + constants.DECODER_TV_NAME, 'w') as f:
+with open(constants.TV_PATH + constants.TV_NAME.get(FILENAME), 'w') as f:
     for i in range(0, MAX_BIT_VALUE + 1):
         output = modules.Decoder.decoder(i, MAX_BIT_VALUE_OUTPUT.bit_length())
 
