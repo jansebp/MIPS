9766d027cdebd4cf87d43e486e4a82738e65cd64
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-23 03:27:25 -0300

    Refatoração das constantes utilizadas no programa

--- utils/constants.py
+++ utils/constants.py
@@ -9,24 +9,36 @@ TV_PATH = '..\\simulation\\modelsim\\'
 TB_PATH = '..\\testbench\\'
 
 # Nomes para os arquivos de test vector (.TV)
-INV_TV_NAME = 'inv.tv'
-TRISTATE_TV_NAME = 'tristate.tv'
-MUX_TV_NAME = 'mux.tv'
-MUX4_TV_NAME = 'mux4.tv'
-FLOPR_TV_NAME = 'flopr.tv'
-FLOPENR_TV_NAME = 'flopenr.tv'
-DECODER_TV_NAME = 'decoder.tv'
+TV_NAME = {
+    'inv': 'inv.tv',
+    'tristate': 'tristate.tv',
+    'mux': 'mux.tv',
+    'mux4': 'mux4.tv',
+    'flopr': 'flopr.tv',
+    'flopenr': 'flopenr.tv',
+    'decoder': 'decoder.tv',
+    'adder': 'adder.tv',
+    'sub': 'sub.tv',
+    'ula': 'ula.tv',
+    'uladec': 'uladec.tv'
+}
 
-MUX_SELECTION_RANGE = 2         # Range de valores para o input 'sel' do MUX
-MUX4_SELECTION_RANGE = 4        # Range de valores para o input 'sel' do MUX4
+RANGES = {
+    'mux': 2,       # Range de valores para o input 'sel' do MUX
+    'mux4': 4,      # Range de valores para o input 'sel' do MUX4
+    'adder': 2,     # Range de valores para o input 'cin' do ADDER
+    'sub': 2,       # Range de valores para o input 'cin' do SUB
+    'enable': 2,    # Range de valores para o Enable
+    'reset': 2      # Range de valores para o Reset
+}
 
-ENABLE_RANGE = 2                # Range de valores para o Enable
-RESET_RANGE = 2                 # Range de valores para o Reset
 CLOCK_PERIOD = 2                # Tempo de execucao para o CLK sair de HIGH e voltar para HIGH
 
-MAX_VALUE_BITS_1 = 1            # Constante para execucao com inputs de 1 bit
-MAX_VALUE_BITS_2 = 3            # Constante para execucao com inputs de 2 bits
-MAX_VALUE_BITS_3 = 7            # Constante para execucao com inputs de 3 bits
-MAX_VALUE_BITS_4 = 15           # Constante para execucao com inputs de 4 bits
-MAX_VALUE_BITS_5 = 31           # Constante para execucao com inputs de 5 bits
-MAX_VALUE_BITS_32 = 4294967295  # Constante para execucao com inputs de 32 bits
+MAX_VALUE_BITS = {
+    1: 1,                       # Constante para execucao com inputs de 1 bit
+    2: 3,                       # Constante para execucao com inputs de 2 bits
+    3: 7,                       # Constante para execucao com inputs de 3 bits
+    4: 15,                      # Constante para execucao com inputs de 4 bits
+    5: 31,                      # Constante para execucao com inputs de 5 bits
+    32: 4294967295              # Constante para execucao com inputs de 32 bits
+}
