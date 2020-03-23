69381072fd064fd72c2c24fd8ba4e41f5fa43674
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-22 21:40:49 -0300

    Implementação do Módulo Registrador (FLOPR) - Script em Python do Golden Model, para gerar o Test Vector - Arquivo .TV gerado - Módulo INV em System Verilog - Arquivo de Testbench, em System Verilog.

---
+++ flopr/flopr.sv
@@ -0,0 +1,10 @@
+module flopr(
+    input logic d,
+    input logic clk,
+    input logic reset,
+    output logic q);
+
+    always_ff @(posedge clk, posedge reset)
+        if (reset) q <= 0;
+        else q <= d;
+endmodule
---
+++ flopr/goldenmodel/flopr_gm.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+from utils import constants, modules
+
+pathlib.Path(constants.TV_PATH).mkdir(parents=True, exist_ok=True)
+pathlib.Path(constants.TB_PATH).mkdir(parents=True, exist_ok=True)
+
+MAX_BIT_VALUE = constants.MAX_VALUE_BITS_1
+formatter = '0' + str(MAX_BIT_VALUE.bit_length()) + 'b'
+
+with open(constants.TV_PATH + constants.FLOPR_TV_NAME, 'w+') as f:
+    clk = 0
+    q = 'x'
+
+    for i in range(0, 2):
+        if i == 0:
+            RESET_VALUE = 0
+            for d in range(0, MAX_BIT_VALUE + 1):
+                for aux in range(0, 2):
+                    for aux2 in range(0, constants.CLOCK_PERIOD):
+
+                        q = modules.Flop.flopr(d, clk, RESET_VALUE, q)
+
+                        if q == 'x':
+                            f.write(format(d, formatter) + constants.DELIMITER + str(clk) + constants.DELIMITER
+                                    + str(RESET_VALUE) + constants.DELIMITER + str('x' * MAX_BIT_VALUE.bit_length())
+                                    + '\n')
+                        else:
+                            f.write(format(d, formatter) + constants.DELIMITER + str(clk) + constants.DELIMITER
+                                    + str(RESET_VALUE) + constants.DELIMITER + format(q, formatter) + '\n')
+
+                        clk = int(not clk)
+        else:
+            for d in range(0, MAX_BIT_VALUE + 1):
+                for aux in range(0, 2):
+                    for reset in range(0, constants.RESET_RANGE):
+                        for aux2 in range(0, constants.CLOCK_PERIOD):
+
+                            q = modules.Flop.flopr(d, clk, reset, q)
+
+                            if q == 'x':
+                                f.write(format(d, formatter) + constants.DELIMITER + str(clk) + constants.DELIMITER
+                                        + str(reset) + constants.DELIMITER + str('x' * MAX_BIT_VALUE.bit_length())
+                                        + '\n')
+                            else:
+                                f.write(format(d, formatter) + constants.DELIMITER + str(clk) + constants.DELIMITER
+                                        + str(reset) + constants.DELIMITER + format(q, formatter) + '\n')
+
+                            clk = int(not clk)
