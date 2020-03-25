f3c683970f75c7a238aa36db02d53d50b228c4f2
Jansepetrus Brasileiro Pereira <jansebp@gmail.com>
2020-03-25 03:35:11 -0300

    Adição da ULA ao arquivo; melhoria no controle da quantidade de bits que cada módulo utiliza para input e output, sem necessitar alterar o arquivo do módulo em si; generalização da forma de recuperar a quantidade de valores possíveis para os bits.

--- utils/constants.py
+++ utils/constants.py
@@ -29,18 +29,37 @@ RANGES = {
     'adder': 2,     # Range de valores para o input 'cin' do ADDER
     'sub': 2,       # Range de valores para o input 'cin' do SUB
     'ula': 2,       # Range de valores para o input 'cin' da ULA
-    'ula_op': 8,     # Range de valores de OP para a ULA
+    'ula_op': 8,    # Range de valores de OP para a ULA
     'enable': 2,    # Range de valores para o Enable
     'reset': 2      # Range de valores para o Reset
 }
 
 CLOCK_PERIOD = 2                # Tempo de execucao para o CLK sair de HIGH e voltar para HIGH
 
-MAX_VALUE_BITS = {
-    1: 1,                       # Constante para execucao com inputs de 1 bit
-    2: 3,                       # Constante para execucao com inputs de 2 bits
-    3: 7,                       # Constante para execucao com inputs de 3 bits
-    4: 15,                      # Constante para execucao com inputs de 4 bits
-    5: 31,                      # Constante para execucao com inputs de 5 bits
-    32: 4294967295              # Constante para execucao com inputs de 32 bits
+# Quantidade de bits do input de cada módulo
+N_BITS_INPUT = {
+    'adder': 1,
+    'decoder': 5,
+    'flopenr': 1,
+    'flopr': 1,
+    'inv': 1,
+    'mux': 1,
+    'mux4': 1,
+    'sub': 1,
+    'tristate': 1,
+    'ula': 32
+}
+
+# Quantidade de bits do output de cada módulo
+N_BITS_OUTPUT = {
+    'adder': 1,
+    'decoder': 32,
+    'flopenr': 1,
+    'flopr': 1,
+    'inv': 1,
+    'mux': 1,
+    'mux4': 1,
+    'sub': 1,
+    'tristate': 1,
+    'ula': 32
 }
---
+++ utils/util.py
@@ -0,0 +1,2 @@
+def qt_numbers_bit(bits):
+    return pow(2, bits) - 1
